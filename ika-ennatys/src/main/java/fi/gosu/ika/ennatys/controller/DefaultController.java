8175688d27c49c7c27df24a91bfae098f5154603
Ville Nupponen <aikain@gosu.fi>
2017-03-02 04:35:47 +0200

    Override default error controller

--- ika-ennatys/src/main/java/fi/gosu/ika/ennatys/controller/DefaultController.java
+++ ika-ennatys/src/main/java/fi/gosu/ika/ennatys/controller/DefaultController.java
@@ -2,16 +2,20 @@ package fi.gosu.ika.ennatys.controller;
 
 import fi.gosu.ika.ennatys.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
 @RequestMapping("/*")
-public class DefaultController {
+public class DefaultController implements ErrorController {
+
+    private static final String PATH = "/error";
 
     @Autowired
     private CategoryRepository categoryRepository;
@@ -31,4 +35,14 @@ public class DefaultController {
         return "forward:/resources/favicon.ico";
     }
 
+    @RequestMapping(value = PATH)
+    public String error(RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("error", "Tuntematon virhe! (tai viallinen päivämäärä)");
+        return "redirect:/";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
 }
